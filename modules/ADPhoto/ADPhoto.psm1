f894bd5e8cd115a6dcf6e4231c22bb8459f5ba82
Daniel Ayo <DanielAyo@users.noreply.github.com>
2017-10-25 13:26:29 +0100

    Changed Get-O365Photo function to use EWS REST API

--- modules/ADPhoto/ADPhoto.psm1
+++ modules/ADPhoto/ADPhoto.psm1
@@ -70,32 +70,48 @@ function Set-ADPhoto {
 function Get-O365Photo {
     <#
     .SYNOPSIS
-    Returns all existing Exchange Online users with photos as JPEG output.
-    
+    Imports User Profile Picture from Exchange Online
     .DESCRIPTION
-    Long description
-    
+    Extracts user photo from Exchange Online using EWS API.
     .EXAMPLE
-    Get-O365Photo -Directory C:\users\test\desktop\Photo 
-    
+    Get-O365Photo -Mail user@contoso.com -Directory C:\Temp -Credential "user@contoso.com"
+    Extracts user photo from Exchange Online and saves it in the path specfied as Name.jpg
+    .EXAMPLE
+    "user1@contoso.com" , "user2@contoso.com" | Get-O365Photo -Directory C:\temp -Credential "user@contoso.com"
+    Extracts multiple user photos and saves it in the path specfied
     .NOTES
-    Must connect to Exchange Online session prior to use.
+    https://msdn.microsoft.com/en-us/library/office/jj190905(v=exchg.150).aspx
     #>
-    [CmdletBinding()]
     param (
-        [Parameter(ValueFromPipelineByPropertyName)]
-        [ValidateNotNullOrEmpty]
-        [string]$Directory = "C:\temp\Photos"
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string] $Mail,
+        [ValidateSet("96x96", "240x240", "648x648")]
+        [string] $Size = "648x648",
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName,
+            HelpMessage = 'Enter the directory where the photo will be stored')]
+        [string] $Directory,
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [pscredential]
+        [System.Management.Automation.CredentialAttribute()]
+        $Credential
     )
-    begin {
-    }
+    begin {}
     process {
-        $photoUsers = get-mailbox -ResultSize Unlimited | Where-Object {$_.HasPicture -eq $True} | Select-Object -ExpandProperty UserPrincipalName
-        foreach ($photoUser in $photoUsers) {
-            $user = Get-UserPhoto $photoUser
-            $user.PictureData | Set-Content "$Directory\$($User.Identity).jpg" -Encoding byte
+        try {
+            $adObj = ([adsisearcher]"(&(objectclass=user)(objectclass=person)(mail=$Mail))").FindOne().GetDirectoryEntry()
+            Write-Verbose "$($adObj.Name) extracted" 
+            $uri = [string]::Concat("https://outlook.office365.com/ews/Exchange.asmx/s/GetUserPhoto?email=", $Mail, "&size=HR", $Size)
+            if (Test-Path -Path $Directory) {
+                $File = [string]::Concat($Directory, "\", $adObj.Name, ".jpg")
+                Invoke-WebRequest -Uri $uri -Credential $Credential -OutFile $File
+            }
+            else {
+                Write-Warning "Please check that the path exists prior to running the function"
+            }
+        }
+        catch {
+            $_.Exception.Message 
         }
     }
-    end {
-    }
+    end {}
 }
