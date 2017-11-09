1ca5a0633c7d1cd75ff1f2aae157a99dc7db8a6c
Daniel Ayo <DanielAyo@users.noreply.github.com>
2017-11-09 13:30:32 +0000

    Added new functions to photo module

--- modules/ADPhoto/ADPhoto.psm1
+++ modules/ADPhoto/ADPhoto.psm1
@@ -115,3 +115,273 @@ function Get-O365Photo {
     }
     end {}
 }
+function Get-PhotoJPEG {
+    <#
+    .SYNOPSIS
+    Retrieves photo names from a photo store
+    
+    .DESCRIPTION
+    Obtians the photos from a specified filestore, checks against on-prem AD users and only returns photos that have a name 
+    match found in on-prem AD
+    
+    .EXAMPLE
+    Get-PhotoJPEG
+    
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName,
+            HelpMessage = 'Enter the directory where the photo is stored')]
+        [ValidateNotNullOrEmpty()]
+        [string]$Directory,
+        [Parameter(ValueFromPipelineByPropertyName,
+            HelpMessage = 'Enter the directory where the logs will be stored')]
+        [string]$LogPath = $env:temp 
+    )
+    begin {
+        $StopWatch = New-Object -TypeName System.Diagnostics.Stopwatch
+        $StopWatch.Start()
+        $start = [DateTime]::Now
+        Write-Verbose "Process Started: $start" 
+        [nullable[double]]$secondsRemaining = $null
+    }
+    process {
+        $photos = Get-ChildItem -Path $Directory -Filter "*.jpg"
+        $total = $photos.count
+        $baseNames = ($photos.BaseName)
+        Write-Verbose "$total photos found"       
+        $acceptedNames = [System.Collections.ArrayList] @()
+        $badNames = [System.Collections.ArrayList] @()
+        $counter = 0
+        $activity = 'Processing Names'
+        foreach ($name in $baseNames) {
+            try {
+                $counter++
+                $percentComplete = $counter / $total * 100
+                $progressParams = @{
+                    Activity         = $activity
+                    Status           = "[$counter / $total] - $([math]::Round($percentComplete))%"
+                    CurrentOperation = "Name: $name"
+                    PercentComplete  = $percentComplete
+                }
+                # if we have an estimate for the time remaining, add it to the Write-Progress parameters
+                if ($secondsRemaining) {
+                    $progressParams.SecondsRemaining = $secondsRemaining
+                }
+                Write-Progress @progressParams
+
+                #Set regex patterns to validate "name space surname" only               
+                $regexName = "^[a-z ,.'-]+$"
+                if ($name -match $regexName) {
+                    [void]$acceptedNames.Add($name)
+                }
+                else {
+                    [void]$badNames.Add($name)
+                }
+                # estimate the time remainingcounter
+                $secondsElapsed = [DateTime]::Now - $start
+                $secondsRemaining = ($secondsElapsed.TotalSeconds / $counter) * ($total - $counter)
+            }
+            catch {
+                Write-Warning $_
+            }
+        }
+        Write-Progress -Activity $activity -Completed
+        if ($badNames.Count -gt 0 ) {
+            Write-Verbose "$($badNames.Count) unusuable names found > $LogPath\UnusableNames.csv"
+            $badNames |  Out-File -FilePath "$LogPath\UnusableNames.csv"
+            #attempt to remove the 0X... format from bad names
+            $fixedNames = foreach ($n in $badNames) {
+                $split = $n -split '\s'
+                $final = $split | ForEach-Object { $_ | Select-String -Pattern "^[a-z,.'-]+$"}
+                $final = $final -join ' '
+                $final
+            }
+            #add the results into a single collection
+            $acceptedNames = $acceptedNames + $fixedNames
+        }
+        #Get all AD users
+        try {
+            $adNames = Get-ADUser -Filter {UserPrincipalName -like '*'} -ErrorAction Stop | Select-Object -ExpandProperty Name | Sort-Object
+            Write-Verbose "$($adNames.count) AD users found"
+        }
+        catch {
+            Write-Warning $_
+            break
+        }
+
+        #compare AD names against sateon name list
+        Write-Verbose "Comparing photo names and AD user object names"
+        $validatedNames = Compare-Object -ReferenceObject $acceptedNames -DifferenceObject $adNames -IncludeEqual -ExcludeDifferent |
+            Select-Object -ExpandProperty InputObject |
+            Sort-Object
+        
+        Write-Verbose "$($validatedNames.count) users common in both databases"
+        #Make note of any duplicate names that appear and export a log file
+        $groupedNames = $validatedNames | Group-Object
+
+        $duplicates = $groupedNames | Where-object {$_.Count -gt 1 } | Select-Object -ExpandProperty Name
+        if ($duplicates) {
+            Write-Verbose "$($duplicates.Count) duplicate name(s) found > $LogPath\UnusableNames.csv"
+            $duplicates | Out-File -FilePath "$LogPath\UnusableNames.csv" -Append
+        }
+        else {
+            Write-Verbose "No duplicates found"
+        }
+        #singleton only users
+        $singletonNames = $groupedNames | Where-object {$_.Count -eq 1 } | Select-Object -ExpandProperty Name
+        Write-Verbose 'Keeping only photos names that are referenced in both databases'
+        #keep only photos that can be associated with on-prem AD users
+        $adPhotoUsers = `
+            foreach ($val in $singletonNames) {
+            foreach ($photo in $photos) { 
+                #making sure the photo name matches the ad name regardless of any additional metadata on the photo name
+                if ($photo.BaseName -match $val) {
+                    $photo
+                    break
+                }
+            }
+        }
+        Write-Verbose "$($adPhotoUsers.Count) photos matched with on-prem AD users > $LogPath\ADPhotoUsers.csv"
+        $adPhotoUsers | Out-File -FilePath "$LogPath\ADPhotoUsers.csv"
+        #Output
+        $adPhotoUsers
+    }
+    end {
+        $StopWatch.Stop()
+        Write-Verbose "Get-PhotoJPEG Process time: $($StopWatch.Elapsed.TotalSeconds.ToString()) seconds"
+    }
+}
+function Connect-ExchangeOnline {
+    <#
+    .SYNOPSIS
+    Connect to Exchange Online tenant
+    
+    .DESCRIPTION
+    Long description
+    
+    .EXAMPLE
+    An example
+    
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName,
+            HelpMessage = 'Enter your O365 credentials')]
+        [pscredential]
+        [System.Management.Automation.CredentialAttribute()]
+        $Credential
+    )
+    #Creates an Exchange Online session using defined credential which allows the import of larger photo files.
+    $ExchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/?proxyMethod=RPS" -Credential $Credential -Authentication Basic -AllowRedirection
+    Import-PSSession $ExchangeSession
+}
+function Set-O365Photo {
+    <#
+    .SYNOPSIS
+    Sets the photo for the Exchange Mailbox of a user
+
+    .DESCRIPTION
+    Long description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    Must connect to Exchange Online session prior to use
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [PSCustomObject[]]$Photos,
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string]$Office = "*",
+        [Parameter(ValueFromPipelineByPropertyName,
+            HelpMessage = 'Enter the directory where the logs will be stored')]
+        [string]$LogPath = $env:TEMP       
+    )
+    begin {
+        $StopWatch = New-Object -TypeName System.Diagnostics.Stopwatch
+        $StopWatch.Start()
+        $start = [DateTime]::Now
+        Write-Verbose "Process Started: $start"
+        $results = [System.Collections.ArrayList] @()
+    }
+    process {
+        $o365Users = Get-Mailbox -ResultSize unlimited | Where-Object {$_.Office -match $Office} |
+            Select-Object Name, HasPicture, IsDirSynced, UserPrincipalName, Office
+        #get 0365 users with no photo
+        $noPhotoUsers = $o365Users | Where-Object {$_.HasPicture -ne $True}
+        $total = $(@($noPhotoUsers).Count)
+        Write-Verbose "$total user(s) with no photo"
+        $activity = 'Processing Photos'
+        $counter = 0
+        foreach ($user in $noPhotoUsers) {
+            $properties = [ordered]@{
+                Name     = ''
+                Username = ''
+                Office   = ''
+                OnPrem   = $false
+                Changed  = $false
+                Photo    = ''
+            }
+            
+            #check if the o365 user is synced with on-Prem AD
+            $counter++
+            $percentComplete = $counter / $total * 100
+            $progressParams = @{
+                Activity         = $activity
+                Status           = "[$counter / $total] - $([math]::Round($percentComplete))%"
+                CurrentOperation = "Name: $($user.Name)"
+                PercentComplete  = $percentComplete
+            }
+            # if we have an estimate for the time remaining, add it to the Write-Progress parameters
+            if ($secondsRemaining) {
+                $progressParams.SecondsRemaining = $secondsRemaining
+            }
+            Write-Progress @progressParams
+
+            $properties.Name = $user.Name
+            $properties.Username = $user.UserPrincipalName
+            $properties.Office = $user.Office
+
+            if ($user.IsdirSynced) {
+                $properties.OnPrem = $true
+                #loop through photos to find name match
+                foreach ($photo in $Photos) {
+                    if ($photo.basename -match $user.Name) {
+                        #If they match - set that photo, for that user
+                        try {
+                            Set-UserPhoto -Identity $user.UserPrincipalName -PictureData ([System.IO.File]::ReadAllBytes($photo.fullname)) -confirm:$False -ErrorAction Stop
+                            if ([bool](Get-UserPhoto -Identity $user.Name)) {
+                                Write-Verbose "$($user.Name) photo set"
+                                $properties.Changed = $true
+                                $properties.Photo = $photo.fullname
+                                break
+                            }
+                        }
+                        catch {
+                            Write-Warning $_
+                        }
+                    }
+                }
+            }
+
+            $objectRow = New-Object -TypeName PsObject -Property $properties
+            [void]$results.Add($objectRow)
+            
+            # estimate the time remainingcounter
+            $secondsElapsed = [DateTime]::Now - $start
+            $secondsRemaining = ($secondsElapsed.TotalSeconds / $counter) * ($total - $counter)    
+        }
+        $results
+    }
+    end {
+        $StopWatch.Stop()
+        Write-Verbose "Set-O365Photo Process time: $($StopWatch.Elapsed.TotalSeconds.ToString()) seconds"
+    }
+}
